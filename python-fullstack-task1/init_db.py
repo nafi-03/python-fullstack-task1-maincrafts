844d497446ea75e8d32b68b7951250a6ebd6e604
nafi-03 <152777876+nafi-03@users.noreply.github.com>
2026-01-02 11:23:53 +0530

    Add files via upload

---
+++ python-fullstack-task1/init_db.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+
+conn.execute('CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)')
+
+conn.close()
+print("Database and 'users' table created successfully!")
